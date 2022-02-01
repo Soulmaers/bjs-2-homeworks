82c1f79eb7d0d8137ae150e4b6e829347e8a8014
Soulmaers <certiss@ya.ru>
2022-02-01 15:49:02 +0300

    Задача 1.2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,18 @@
 'use strict'
 function solveEquation(a, b, c) {
-  let arr;
 
   let d = b * b - 4 * a * c;
   if (d < 0) {
-    return arr = [];
+    return [];
   }
   if (d === 0) {
     let x = -b / (2 * a);
-    return arr = [x];
+    return [x];
   }
   if (d > 0) {
     let x1 = (-b + Math.sqrt(d)) / (2 * a);
     let x2 = (-b - Math.sqrt(d)) / (2 * a);
-    return arr = [x1, x2];
+    return [x1, x2];
   }
 
   return arr; // array
@@ -22,11 +21,16 @@ function solveEquation(a, b, c) {
 
 
 
-
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
+  let nowTime = new Date();
+  let oneDay = 1000 * 60 * 60 * 24;
+  let kreditDate = parseInt((date.getTime() - nowTime.getTime()) / oneDay * 30);
+  let kreditBody = parseInt(amount - contribution);
+  let p = parseInt(percent / 12 / 100);
+  let payment = kreditBody * (p + (p / (((1 + p) ^ kreditDate) - 1)))
+  let totalAmount = parseInt((payment * kreditDate).toFixed(2));
 
-  return totalAmount;
+
+  return console.log(totalAmount);
 }
