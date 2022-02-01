71d1f745c3c6f2fe60d066f3c290849bccf48216
Soulmaers <certiss@ya.ru>
2022-02-01 11:16:05 +0300

    Первая задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,27 @@
+'use strict'
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+
+  let d = b * b - 4 * a * c;
+  if (d < 0) {
+    return arr = [];
+  }
+  if (d === 0) {
+    let x = -b / (2 * a);
+    return arr = [x];
+  }
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    return arr = [x1, x2];
+  }
+
   return arr; // array
 }
+
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
