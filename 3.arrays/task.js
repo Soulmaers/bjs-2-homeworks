e34259fb7fa3e311d4688eacdcb90dfdc9b212fe
Soulmaers <certiss@ya.ru>
2022-02-17 15:53:44 +0300

    "Array"

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = arr1.length === arr2.length && arr1.every((item, i) => arr2[i] === item);
 
-  // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter((item) => item > 0)
+    .filter((item) => item % 3 === 0)
+    .map((item) => item * 10);
 
-  // Ваш код
+  return resultArr;
 
-  return resultArr; // array
 }
+// array
