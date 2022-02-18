9ad747fb0ade36e4fa6d663179a0a40c6ae01e57
Soulmaers <certiss@ya.ru>
2022-02-18 15:43:51 +0300

    "Объекты"

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 
 }
+
+let student01 = new Student('Jonh', 'male', 30);
+let student02 = new Student('Viky', 'female', 25);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+
+
+}
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+
+  }
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function (...mark) {
+  this.marks = [];
+  mark.forEach(el => this.marks.push(el));
+
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach(el => sum += el);
+  return sum / this.marks.length;
+
+}
+
+Student.prototype.exclude = function (reason) {
+
+  delete this.subjectName;
+  delete this.marks;
+  this.excluded = reason;
+}
+
+
