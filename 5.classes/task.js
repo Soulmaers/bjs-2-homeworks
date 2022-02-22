bb2ffa9e0ec4bbdb1eb18c7bda017a11096c1baf
Soulmaers <Certiss@ya.ru>
2022-02-22 14:03:43 +0300

    Классы

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -84,10 +84,9 @@ class Library {
     giveBookByName(bookName) {
         for (let i = 0; i < this.books.length; i++) {
             if (this.books[i].name === bookName)
-               
-            return this.books.splice([i], 1)[0];
+
+                return this.books.splice([i], 1)[0];
         }
-        
         return null
     }
 }
