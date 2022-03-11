ec1ebb72f0029c3794287d18729bab7e4e2e9cec
Soulmaers <certiss@ya.ru>
2022-03-11 14:45:40 +0300

    Готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,72 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+      const hash = args.join(',');
+      let idx = cache.findIndex((el)=> (el['hash']===hash) ); 
+      if (idx !== -1 ) { 
+        let res = cache[idx].res;
+          console.log("Из кэша: " + res); 
+          return "Из кэша: " + res;
+      }
+  
+      let result = func(...args);
+      let object={};
+      object['hash']=hash;
+      object['res']=result;
+      cache.push(object) ; 
+      if (cache.length > 5) { 
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;  
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func,ms) {
+  let timeout;
+  let flag=false;
+  function wrapper(...args){
+    if (flag===false) {
+      func.call(this,...args);
+      flag=true;
+      timeout=setTimeout(()=>{
+        flag=false;
+      },ms)
+    }else{
+    clearTimeout(timeout);
+    timeout=setTimeout(()=>{
+      flag=false;
+    }, ms);
+
+
+    }
+
+  }
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func,ms) {
+  let timeout;
+  let flag=false;
+  wrapper.count=0;
+
+  function wrapper(...args){
+    if (flag==false){
+      func.call(this,...args);
+      flag=true;
+      timeout=setTimeout(()=>{
+        flag=false;
+      },ms);
+    }else{
+      clearTimeout(timeout);
+      timeout=setTimeout(()=>{
+        flag=false;
+      },ms);
+wrapper.count+=1;
+    }
+    return wrapper;
+  }
 }
